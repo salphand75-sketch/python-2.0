dfcf9e00299f2293bd5d48fa8d7a46eae81b5c8e
salphand75-sketch <s.alphand75@gmail.com>
2026-03-09 16:08:39 +0100

    Add files via upload

--- jetlearnHANGMAN.py
+++ jetlearnHANGMAN.py
@@ -75,17 +75,32 @@ _________
 |
 ''']
 countl = 0
+countw = 0
+used_letters = []
 while True:
     print( " ".join(letterlist))
     wletter = input("What letter do you think is in the word? ")
+    if wletter in used_letters:
+        print("You already used that letter!")
+        continue
+    if len(wletter) != 1:
+        print("Please enter only ONE letter.")
+        continue
+    used_letters.append(wletter)
     if wletter in word:
         for i in range (len(word)):
             if wletter == word[i]:
                 letterlist[i] = wletter 
+                countw += 1
+        if "_" not in letterlist:
+            print(" ".join(letterlist))
+            print("You win.")
+            break
     else:
         print("Not in the word!")
-        countl += 1
         print(stages[countl])
-
-
-    
+        countl += 1
+        if countl == len(stages):
+            print("You lose")
+            print("The word was:", word)
+            break    
