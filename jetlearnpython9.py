b0e5db8f17eae8f3f42a88d6b390264cdf77783c
salphand75-sketch <s.alphand75@gmail.com>
2026-03-16 16:05:16 +0100

    Add files via upload

--- jetlearnpython9.py
+++ jetlearnpython9.py
@@ -1,25 +1,27 @@
 import turtle
 import random
-import time
 t = turtle.Turtle()
 s = turtle.Screen()
 turtle.colormode(255)
-s.bgcolor(0,0,0)
-t.speed(0)
-t.hideturtle()
-for i in range(10):
-    for i in range(3):
-        color1 = random.randint(1,255)
-        color2 = random.randint(1,255)
-        color3 = random.randint(1,255)
-        size = random.randint(30,70)
-        t.color(color1,color2,color3)
-        t.begin_fill()
-        t.circle(size)
-        t.end_fill()
-        t.up()
-        t.goto(random.randint(-230,230),random.randint(-230,230))
-        t.down()
-    time.sleep(0.5)
-    t.clear()
-turtle.done()
+t.shape("circle")
+t.up()
+t.goto(-230,0)
+t.left(90)
+for i in range(4):
+    color1 = random.randint(0,255)
+    color2 = random.randint(0,255)
+    color3 = random.randint(0,255)
+    t.color(color1,color2,color3)
+    for i in range(18):
+        t.forward(10)
+        t.right(10)
+    t.left(180)
+for i in range(4):
+    color1 = random.randint(0,255)
+    color2 = random.randint(0,255)
+    color3 = random.randint(0,255)
+    t.color(color1,color2,color3)
+    for i in range(18):
+        t.forward(10)
+        t.left(10)
+    t.right(180)
