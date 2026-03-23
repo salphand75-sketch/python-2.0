ff731c7e7a139b2a3369e9b987b1c231489f5cda
salphand75-sketch <s.alphand75@gmail.com>
2026-03-23 17:07:55 +0100

    Add files via upload

--- jetlearnturtleMAZE.py
+++ jetlearnturtleMAZE.py
@@ -1,37 +1,87 @@
 import turtle
 t = turtle.Turtle()
+t.shape("turtle")
+t.color("green")
+t.shapesize(0.7,0.7)
+t.up()
+t.goto(-176,176)
 food = turtle.Turtle()
 s = turtle.Screen()
 s.setup(500,500)
 t.speed(0)
-maze = ["XXXXXXXXXXXXXXX ",
-        "X             X ",
-        "X X XXXXXXXXX X ",
-        "X X    X      X ",
-        "X XXXX XXXXX XX ",
-        "X   X      X  X ",
-        "XXXXXXXXXX XX X ",
-        "X     X    X  X ",
-        "X XX  XXXXXX XX ",
-        "X XXX   X     X ",
-        "X   X X   XXXXX ",
-        "X X X XXXXX   X ",
-        "X X XXX   X X X ",
-        "X X     X   X X ",
-        "XXXXXXXXXXXXXFX ",]
-for i in range(len(maze)):
-    for j in range(len(maze[i])):
-        x = -200 + (j*25)
-        y = 200 - (i*25)
-        if maze[i][j] == "X":
-            wall = turtle.Turtle()
-            wall.speed(0)
-            wall.shape("square")
-            wall.up()
-            wall.goto(x,y)
-        elif maze[i][j] == "F":
-            food.up()
-            food.shape("circle")
-            food.color("green")
-            food.goto(x,y)
-turtle.done()
+while True:
+    walls = []
+    maze = ["XXXXXXXXXXXXXXX",
+            "X             X",
+            "X X XXXXXXXXX X",
+            "X X    X      X",
+            "X XXXX XXXXX XX",
+            "X   X      X  X",
+            "XXXXXXXXXX XX X",
+            "X    XX    X  X",
+            "X XX  XXXXXX XX",
+            "X XXX   X     X",
+            "X   X X   XXXXX",
+            "X X X XXXXX   X",
+            "X X XXX   X X X",
+            "X X     X   X X",
+            "XXXXXXXXXXXXXFX",]
+    for i in range(len(maze)):
+        for j in range(len(maze[i])):
+            x = -200 + (j*24)
+            y = 200 - (i*24)
+            if maze[i][j] == "X":
+                wall = turtle.Turtle()
+                wall.speed(0)
+                wall.shape("square")
+                wall.up()
+                wall.goto(x,y)
+                walls.append(wall)
+            elif maze[i][j] == "F":
+                food.up()
+                food.shape("circle")
+                food.speed(0)
+                food.color("red")
+                food.goto(x,y)
+    def check_wall(x,y):
+        for wall in walls:
+            if wall.xcor() == x and wall.ycor() == y:
+                return True
+        return False
+    def up():
+        y = (t.ycor()+24)
+        x = t.xcor()
+        if check_wall(x,y) == False:
+            t.sety(y)
+        t.setheading(90)
+        win()
+    s.listen()
+    s.onkey(up,"Up")
+    def right():
+        x = (t.xcor()+24)
+        y = t.ycor()
+        if check_wall(x,y) == False:
+            t.setx(x)
+        t.setheading(0)
+        win()
+    s.onkey(right,"Right")
+    def down():
+        y = (t.ycor()-24)
+        x = t.xcor()
+        if check_wall(x,y) == False:
+            t.sety(y)
+        t.setheading(270)
+        win()
+    s.onkey(down,"Down")
+    def left():
+        x = (t.xcor()-24)
+        y = t.ycor()
+        if check_wall(x,y) == False:
+            t.setx(x)
+        t.setheading(180)
+        win()
+    s.onkey(left,"Left")
+    def win():
+        if t.distance(food) < 20:
+            print("You have eaten your food!")
+    break
