1e1556c792c772f7b7a6b1b203acaf244de550b5
salphand75-sketch <s.alphand75@gmail.com>
2026-05-18 16:11:45 +0200

    Add files via upload

--- jetlearnturtleBalloonPopper.py
+++ jetlearnturtleBalloonPopper.py
@@ -1,13 +1,8 @@
 import turtle
 import random
-import time
 turtle.colormode(255)
 t = turtle.Turtle()
 s = turtle.Screen()
-badballoon = turtle.Turtle()
-badballoon.pencolor("red")
-badballoon.fillcolor("black")
-badballoon.shape("circle")
 showscore = turtle.Turtle()
 showscore.up()
 showscore.hideturtle()
@@ -21,6 +16,24 @@ t.right(30)
 t.up()
 t.goto(-10,-200)
 s.listen()
+player_life1 = turtle.Turtle()
+player_life1.up()
+player_life1.goto(-250,250)
+player_life1.speed(0)
+player_life1.color("red")
+player_life1.shape("arrow")
+player_life2 = turtle.Turtle()
+player_life2.up()
+player_life2.goto(-238,250)
+player_life2.speed(0)
+player_life2.color("red")
+player_life2.shape("arrow")
+player_life3 = turtle.Turtle()
+player_life3.up()
+player_life3.goto(-226,250)
+player_life3.speed(0)
+player_life3.color("red")
+player_life3.shape("arrow")
 def right():
     x = (t.xcor()+25)
     y = t.ycor()
@@ -48,8 +61,40 @@ def spawn_balloon():
     balloon.showturtle()
     balloonlist.append(balloon)
 balloonlist = []
-while True:
-    
+
+def spawn_badballoon():
+    badballoon = turtle.Turtle()
+    badballoon.hideturtle()
+    badballoon.speed(0)
+    badballoon.shape("circle")
+    badballoon.pencolor("red")
+    badballoon.fillcolor("black")
+    badballoon.up()
+    x = random.randint(-250,250)
+    badballoon.goto(x,250)
+    badballoon.showturtle()
+    badballoonlist.append(badballoon)
+badballoonlist = []
+game_on = True
+while game_on:  
+    timespa = random.randint(0,80)
+    if timespa == 9:
+        spawn_badballoon()
+    for badballoon in badballoonlist:
+        badballoon.sety(badballoon.ycor()-5)
+        if t.distance(badballoon) < 20:
+            badballoon.hideturtle()
+            badballoonlist.remove(badballoon)
+            if player_life1.isvisible():
+                player_life1.hideturtle()
+            elif player_life2.isvisible():
+                player_life2.hideturtle()
+            elif player_life3.isvisible():
+                player_life3.hideturtle()
+                turtle.write("Game Over!",font=("arial",20,"bold"))
+                turtle.hideturtle()
+                game_on = False
+                break
     timesp = random.randint(0,40)
     if timesp == 9:
         spawn_balloon()
@@ -61,4 +106,9 @@ while True:
             score = score + 1
             showscore.clear()
             showscore.write("score = " + str(score),font=("Arial",15,"normal"))
+for badballoon in badballoonlist:
+    badballoon.hideturtle()
+for balloon in balloonlist:
+    balloon.hideturtle()
+t.hideturtle()
 turtle.done()
