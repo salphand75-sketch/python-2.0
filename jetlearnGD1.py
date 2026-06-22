e58e7b041c37fdd93bf3554df7fdf39ac19d257c
salphand75-sketch <s.alphand75@gmail.com>
2026-06-22 16:05:24 +0200

    Add files via upload

--- jetlearnGD1.py
+++ jetlearnGD1.py
@@ -1,4 +1,4 @@
-product = {"douzen eggs": 5,"milk": 4,"butter": 3,"bread": 6, "jam": 2}
+product = {"eggs": 5,"milk": 4,"butter": 3,"bread": 6, "jam": 2}
 for key,value in product.items():
     print(key,"is",value,"euro")
 cart = {}
@@ -20,10 +20,9 @@ while dubblebreak:
         else:
             print("please choose y/n")
             continue
-
+total = 0
 for x,y in cart.items():
     calc = y * product[x]
-    print(x,"x",y,calc,"euro") 
-      
-
- 
+    total = total + calc
+    print(x,"x",y,"=",calc,"euro") 
+print("Your total is",total,"euro")
